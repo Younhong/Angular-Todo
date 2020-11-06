4b46563c3c89db0dddf12c70c3a991948d3f38fd
younhong <younhong@kakao.com>
2020-11-06 21:45:31 +0900

    upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-todo';
+  title:String = 'angular-todo';
+  name:String = "Younhong";
+
+  constructor(){
+    this.changeName("Yhong");
+  }
+
+  changeName(name:String) {
+    this.name = name;
+  }
 }
